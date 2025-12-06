7e07765e0d040fc0c6c24cc9ab0bfc77e6d47544
johnkhore5911 <johnkhore26@gmail.com>
2025-12-06 18:46:41 +0530

    sync issue fixed

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
